ce0aaaf984d20f7c9d1ca1883ac2a8a1d463e6f8
Yurina Shin <yurinas@gmail.com>
2018-10-17 17:19:11 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -20,7 +20,7 @@ class LandmarksController < ApplicationController
     @landmark = Landmark.find_by_id(params[:id])
     erb :'landmarks/edit'
   end
-  patch '/ladnmarks/:id' do
+  patch '/ladnmarks' do
 
     @landmark = Landmark.find_by_id(params[:id])
     @landmark.name = params['landmark']['name']
