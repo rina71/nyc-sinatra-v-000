b6072d1c0930d7eebecf92a5c2ea1f7268ff30bd
Yurina Shin <yurinas@gmail.com>
2018-10-17 17:06:51 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,40 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  get '/landmarks' do
+
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  get '/landmarks/:id'do
+
+    @landmark = Landmark.find_by(params[:id])
+
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'landmarks/edit'
+  end
+  post '/landmarks' do
+
+    @landmark = Landmark.create(name: params['landmark']['name'], year_completed: params['landmark']['year_completed'])
+    @landmark.save
+    redirect "/landmarks"
+  end
+  post '/ladnmarks/:id' do
+
+    @landmark = Landmark.find_by_id(params[:id])
+    @landmark.name = params['landmark']['name']
+   @landmark.year_completed = params['landmark']['year_completed']
+   @landmark.save
+
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+
 end
